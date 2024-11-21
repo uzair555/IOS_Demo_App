d9cb8c682af3857cbdd26ab5cee2d59afba6a3d2
uzair555 <koraiu6@gmail.com>
2024-11-21 17:00:36 +0500

    Initial Commit

---
+++ Demo 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo 1
+//
+//  Created by Sharjeel on 21/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo 1/Demo_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_1App.swift
+//  Demo 1
+//
+//  Created by Sharjeel on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Demo_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
