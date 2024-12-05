323c06b9061579d9347236fbcfacb43ac51d82b1
uzair555 <koraiu6@gmail.com>
2024-12-05 17:11:48 +0500

    added menu app

---
+++ Demo 1/MenuItem.swift
@@ -0,0 +1,15 @@
+//
+//  MenuItem.swift
+//  Demo 1
+//
+//  Created by Sharjeel on 04/12/2024.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable{
+    var id: UUID=UUID()
+    var name:String
+    var price:String
+    var imageName:String
+}
--- Demo 1/MenuView.swift
+++ Demo 1/MenuView.swift
@@ -8,91 +8,46 @@
 import SwiftUI
 
 struct MenuView: View {
-  @State var playerCard = "card7"
-   @State var cpuCard = "card14"
-    
-    @State var playerScore = 0
-   @State var cpuScore = 0
-    
+ 
+    var menuItem:[MenuItem]=[MenuItem(name: "Onigiri", price: "1.99", imageName: "onigiri"),
+                             MenuItem(name: "Meguro Sushi", price: "5.99", imageName: "meguro-sushi"),
+                             MenuItem(name: "Tako Sushi", price: "4.99", imageName: "tako-sushi"),
+                             MenuItem(name: "Avocado Maki", price: "2.99", imageName: "avocado-maki"),
+                             MenuItem(name: "Tobiko Spicy Maki", price: "4.99", imageName: "tobiko-spicy-maki"),
+                             MenuItem(name: "Salmon Sushi", price: "4.99", imageName: "salmon-sushi"),
+                             MenuItem(name: "Hamachi Sushi", price: "6.99", imageName: "hamachi-sushi"),
+                             MenuItem(name: "Kani Sushi", price: "3.99", imageName: "kani-sushi"),
+                             MenuItem(name: "Tamago Sushi", price: "3.99", imageName: "tamago-sushi"),
+                             MenuItem(name: "California Roll", price: "3.99", imageName: "california-roll"),
+                             MenuItem(name: "Shrimp Sushi", price: "3.99", imageName: "shrimp-sushi"),
+                             MenuItem(name: "Ikura Sushi", price: "5.99", imageName: "ikura-sushi")]
     
     
     var body: some View {
-        ZStack{
-            Image("background-cloth")
-            // this one
-                .resizable()
-                .ignoresSafeArea()
-            
-//            or this one
-                //.aspectRatio(contentMode: .fit)
-            
-            VStack{
+        List(menuItem){item in
+            HStack{
+                Image(item.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height:50)
+                    .cornerRadius(10)
+                Text(item.name)
+                    .bold()
                 Spacer()
-                Image("logo")
-                Spacer()
-                HStack{
-                    Spacer()
-                    Image(playerCard)
-                    Spacer()
-                    Image(cpuCard)
-                    Spacer()
-                }
-                Spacer()
-                Button {
-                    deal()
-                } label: {
-                    Image("button")
-                }
-
-                Spacer()
-                HStack{
-                    VStack{
-                        Text("Player")
-                            .font(.headline)
-                            .padding(.bottom, 10.0)
-                        Text(String(playerScore))
-                            .font(.largeTitle)
-                        
-                        
-                    }
-                    VStack{
-                        
-                        Text("CPU")
-                            .font(.headline)
-                            .padding(.bottom, 10.0)
-                        Text(String(cpuScore))
-                            .font(.largeTitle)
-                    }
-                   
-                }.foregroundColor(.white)
-            }
+                Text("$"+item.price)
+            }.listRowSeparator(.hidden)
+                .listRowBackground(
+                    Color(.brown)
+                        .opacity(0.1)
+                )
         }
-       
-        
-       
-        
-       
+        .listStyle(.plain)
+        .scrollIndicators(.hidden)
         
         
     }
     
-    func deal(){
-       //randomize player card
-        let playerCardValue=Int.random(in:2...14)
-        playerCard = "card"+String(playerCardValue)
-        
-        //randomize cpu card
-        let cpuCardValue=Int.random(in: 2...14)
-        cpuCard = "card"+String(cpuCardValue)
-        
-        //score update
-        if(playerCardValue>cpuCardValue){
-            playerScore += 1
-        }else if(playerCardValue<cpuCardValue){
-            cpuScore += 1
-        }
-        
-    }
+  
 }
 
 #Preview {
